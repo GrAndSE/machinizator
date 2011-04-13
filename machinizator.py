39bad55f6eb9e74d5e79302e06802f6ba5747953
Andrey Grygoryev <undeadgrandse@gmail.com>
2011-04-13 20:33:40 +0300

    Machinizer is broken and looks like a scratch. Simple test added

--- machinizator.py
+++ machinizator.py
@@ -1,4 +1,35 @@
 """Library provides state machine to controll models
+
+Example usage:
+
+
+    class Test:
+        name = "Andy"
+        state = None
+
+
+    def finish_task(obj, name, from_state, to_state):
+        print "Finish task"
+        print obj, name, from_state, to_state
+
+    def start_task(obj, name, from_state, to_state):
+        print "Start task"
+        print obj, name, from_state, to_state       
+
+
+    class TestStateMachine(StateMachine):
+        for_class = Test
+
+        state = StateProperty('default')
+
+        default = State()
+        working = State(state)
+        waiting = State()
+
+        Event(from_state=default, to_state=working)
+        Event(from_state=default, to_state=waiting)
+        Event(from_state=working, to_state=waiting, task=finish_task)
+
 """
 
 
@@ -12,21 +43,35 @@ class State(object):
     """State representation
     """
 
-    def __init__(self, name, value, 
-                        on_enter=None, on_exit=None,
-                        restrictions=None):
+    def __init__(self, property=None, on_enter=None, on_exit=None):
         """Create new state for specified property name and value
+
+        Arguments:
+            name    property name
         """
         super(State, self).__init__()
-        self.name   = name
-        self.value  = value
+        self.property   = property
+        self.on_enter   = on_enter
+        self.on_exit    = on_exit
+
+    def enter(self, prev_state=None):
+        """Enter the state
+        """
+        if self.on_enter is not None:
+            self.on_enter(self.property, self.name, 
+                          prev_state. self.property.get())
+
+    def exit(self, next_state=None):
+        if self.on_exit is not None:
+            self.on_exit(self.property, self.name, 
+                         self.property.get(), next_state)
 
 
 class Event(object):
     """Event object representatin changing of the state
     """
 
-    def __init__(self, property, from_value, to_value, task):
+    def __init__(self, obj, property, from_value, to_value, task):
         """Create new event instance
 
         Arguments:
@@ -34,55 +79,80 @@ class Event(object):
             start       - start value for this event
             end         - end value for this event
         """
+        self.obj        = obj
         self.property   = property
-        self.from_value = start
-        self.to_value   = end
+        self.from_value = from_value
+        self.to_value   = to_value
         self.task       = task
+
+    def __call__(self):
+        if self.task is not None:
+            self.task(obj, property, from_value, to_value)
 
 
 class StateProperty(object):
     """Property used 
     """
 
-    def __init__(self, obj, property):
+    def __init__(self, obj, property, states=[]):
         """Create new state property for specified object property
         """
-        self.property   = property
         self.obj        = obj
-        self.states     = {}
+        self.property   = property
+        self.states     = {[(state, State())]}
         self.events     = []
 
-    def set(self, value):
-        """Chane
+    def set(self, new_state):
+        """Change state
         """
+        old_state = obj.__dict__[property]
+        # Process exit
+        self.states[old_state].exit(new_state)
+        # Process events
+        for event in self.events:
+            if event.from_value == old_state and event.to_value == new_state:
+                event()
+        # Process enter
+        self.states[new_state].enter(new_state)
 
 
-class StateMachineBase(type):
-    """Metaclass for all the state machine classes
+def add_state_properties(cls, property, initial_value):
+    """Add new state property into list
+
+    Arguments:
+        property        original property
+        initial_value   default value
     """
-
-    def __new__(cls, name, bases, attrs):
-        """Create new class
-        """
-        super_new = super(StateMachineBase, cls).__new__
-        parents = [b for b in bases if isinstance(b, StateMachineBase)]
-        if not parents:
-            # If this isn't a subclass of Model, don't do anything special.
-            return super_new(cls, name, bases, attrs)
-        # Get all
-
-        return
-
-
-class StateMachine(object):
-    """Class representing the state machine
-    """
-    __metaclass__ = StateMachineBase
-
-    def __setattr__(self, name, value):
-        """Set attribute with state switching
-        """
-        if name is self.__states:
-            self.__states[name].set(value)
+    # Get hidden properties
+    properties_alias = '_%s__state_properties' % cls.__name__
+    if properties_alias not in cls.__dict__:
+        cls.__dict__[properties_alias] = {}
+    properties = cls.__dict__[properties_alias]
+    # Check for dublicate
+    if isinstance(property, str) or issubclass(property.__class__, str):
+        properties[property] = StateProperty()
+    else:
+        if '_uninitializaed_state_properties' not in cls.__dict__:
+            cls.__dict__['_uninitializaed_state_properties'] = [property,]
         else:
-            super(StateMachine, self).__setattr__(name, value)
+            cls.__dict__['_uninitializaed_state_properties'].append(property)
+
+
+
+def StateMachine(cls, **args):
+    """Create class represents state machine
+    """
+    super_new = super(cls, cls).__new__
+    # Get all
+    new_atts = {cls.__dict__}
+    states = {}
+    events = {}
+    for arg_name in args:
+        arg = args[arg_name]
+        if isinstance(arg, State) or issubclass(arg.__class__, State):
+            states[arg_name] = arg
+        elif isinstance(arg, Event) or issubclass(arg.__class__, Event):
+            new_atts[attr_name] = attr
+    new_atts['__states'] = states
+    print new_atts
+    return super_new(cls, name, bases, new_atts)
---
+++ test.py
@@ -0,0 +1,28 @@
+from machinizer import *
+
+class Test:
+    name = "Andy"
+    state = None
+
+
+def finish_task(obj, name, from_state, to_state):
+    print "Finish task"
+    print obj, name, from_state, to_state
+
+def start_task(obj, name, from_state, to_state):
+    print "Start task"
+    print obj, name, from_state, to_state       
+
+
+class TestStateMachine(StateMachine):
+    for_class = Test
+
+    state = StateProperty('default')
+
+    default = State()
+    working = State(state)
+    waiting = State()
+
+    Event(from_state=default, to_state=working)
+    Event(from_state=default, to_state=waiting)
+    Event(from_state=working, to_state=waiting, task=finish_task)
